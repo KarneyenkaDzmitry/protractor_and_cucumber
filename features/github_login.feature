84f5b5e7c2aa5009a0ce2b1ced898201999ec174
Dzmitry_Karneyenka <Dzmitry_Karneyenka@epam.com>
2018-07-13 13:33:09 +0300

    I have done two scenario

--- features/github_login.feature
+++ features/github_login.feature
@@ -3,14 +3,14 @@ Feature: logit on github web site
   Given I am on page "https://github.com/"
    When I click on "Sing in" reference at the top of page
    Then I see the "Sign in to GitHub" text 
-   And  I see the form with "2" fields
-#     And I see the field "Username or email address" and the field "Password"
+    And  I see the form with "2" fields
+    And I see the field "Username or email address" and the field "Password"
  
-#  Scenario: login with existing user
-#   Given I am on page "https://github.com/"
-#    When I click on "Sing in" reference at the top of page
-#     And I login on the site with options
-# |username|UserForTests|
-# |password|  GitTest_1 |
-#    Then I see the homepage of the user with text "UserForTests"
+ Scenario: login with existing user
+  Given I am on page "https://github.com/"
+   When I click on "Sing in" reference at the top of page
+    And I login on the site with options
+|username|UserForTests|
+|password|  GitTest_1 |
+  Then I see the homepage of the user with button "New repository"
   
--- features/step_definitions/github_login_steps.js
+++ features/step_definitions/github_login_steps.js
@@ -2,35 +2,37 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const {Given, When, Then} = require('cucumber');
+const { Given, When, Then } = require('cucumber');
 
-    Given('I am on page {string}', string => {
-        browser.get(string);
-    });
-    When('I click on {string} reference at the top of page',  (string) => {
-        element(by.css('a[href="/login"]')).click();
-    });
-    Then('I see the {string} text',async function (string) {
-        const text = await element(by.css('form h1')).getText();
+Given('I am on page {string}', async string => {
+    await browser.get(string);
+    //await browser.driver.sleep(5000);
+});
+When('I click on {string} reference at the top of page',async  (string) => {
+    await element(by.css('a[href="/login"]')).click();
+});
+Then('I see the {string} text', { timeout: 2 * 5000 }, async function (string) {
+    const text = await element(by.css('form h1')).getText();
+    expect(text).to.be.equal(string);
+});
+Then('I see the form with {string} fields', async function (string) {
+    const fields = await element.all(by.css('form input[id]'));
+    return expect(fields.length).to.be.equal(Number(string));
+});
+Then('I see the field {string} and the field {string}', async function (string, string2) {
+    const textsAboveFields = [await element(by.css('label[for^=login]')).getText(), await element(by.css('label[for^=pass]')).getText()];
+    await expect(textsAboveFields.indexOf(string)).to.not.is.equal(-1);
+});
+
+When('I login on the site with options', { timeout: 4 * 5000 }, async function (dataTable) {
+    const data = dataTable.rowsHash();
+    const nameField = await element(by.css('input[name=login]'));
+    const passwordField = await element(by.css('input[name=password]'));
+    await nameField.sendKeys(data.username);
+    await passwordField.sendKeys(data.password);
+    await element(by.css('input[value="Sign in"]')).click();
+});
+    Then('I see the homepage of the user with button {string}', async function (string) {
+        const text = await element(by.css('a[href="/new"][class^=btn]')).getText();
         expect(text).to.be.equal(string);
     });
-    Then('I see the form with {string} fields', async function (string) {
-        const fields = await element.all(by.css('form input[id]'));
-        return expect(fields.length).to.be.equal(Number(string));
-    });
-    // Then('I see the field {string} and the field {string}', function (string, string2) {
-    //     console.log('Then I see the field {string} and the field {string}');// Write code here that turns the phrase above into concrete actions
-    //     return 'pending';
-    // });
-    // // When('I click on {string} reference at the top of page', function (string) {
-    // //     console.log('When I click on {string} reference at the top of page');// Write code here that turns the phrase above into concrete actions
-    // //     return 'pending';
-    // // });
-    // When('I login on the site with options', function (dataTable) {
-    //     console.log('When I login on the site with options');// Write code here that turns the phrase above into concrete actions
-    //     return 'pending';
-    // });
-    // Then('I see the homepage of the user with text {string}', function (string) {
-    //     console.log('Then I see the homepage of the user with text {string}');// Write code here that turns the phrase above into concrete actions
-    //     return 'pending';
-    // });
